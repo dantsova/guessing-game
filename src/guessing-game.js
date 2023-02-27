0a30b669b3b64e9c5f21fe244ad29e3bf55fae53
dantsova <yanadontsv@gmail.com>
2023-02-27 16:10:39 +0300

    feat: implemented сlass GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.guessInt = null;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        this.guessInt = Math.ceil((this.min + this.max) / 2);
+        return this.guessInt;
     }
 
     lower() {
-
+        this.max=this.guessInt;
+        return this.max;
     }
 
     greater() {
-
+        this.min=this.guessInt;
+        return this.min;
     }
 }
 
